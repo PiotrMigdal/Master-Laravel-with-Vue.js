9e297aaf01c61e138113389a3ab35aac044b1061
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-07-19 07:48:39 +0100

    End because incorrect version

---
+++ .history/laravelbnbD/database/factories/BookableFactory_20210708071801.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\bookable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookableFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = bookable::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
